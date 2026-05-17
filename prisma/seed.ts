d41d300e8a02d26cec4f9654c26729c720e97dd6
João Pedro <joaopoliveirapaulino@gmail.com>
2026-05-17 14:52:41 -0300

    chore: add shadcn setup

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,9 @@
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
+import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+dotenv.config();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 async function seedDatabase() {
   try {
